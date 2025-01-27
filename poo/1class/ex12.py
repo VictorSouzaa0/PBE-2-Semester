27ef93bc587ad2d51f304ddd56d4992277a6f76f
VictorSouzaa0 <victor.r.souzaa05@gmail.com>
2025-01-27 02:52:32 -0300

    ex12u

--- poo/1class/ex12.py
+++ poo/1class/ex12.py
@@ -1,3 +1,20 @@
 class Ecommerece():
     def __init__(self,products):
         self.products = products
+    prod = []
+    copy = []
+    def singupProd(self,):
+        prod = []
+        entryWhile = 'S'
+        while entryWhile == 'S':
+            self.products = input('Cadastre o produto desejado : ')
+            prod.append(self.products)
+            print (f'{self.products} adicionado com sucesso na lista deseja continuar? S/N?')
+            entryWhile = input('Insira a opção: ')
+        
+        print (f'Produtos cadastrados: {prod}')
+
+prod1 = Ecommerece(products=any)
+print(prod1.singupProd())
+
+#Irei terminar essa lista até o fim dessa semana
